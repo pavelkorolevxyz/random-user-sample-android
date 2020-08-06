e6b1da69720f48e1dc99cb6844983ed0bce7c45d
Pavel Korolev <king64rus@gmail.com>
2020-08-06 22:34:33 +0300

    Docs added to core module.

--- core/src/main/java/xyz/pavelkorolev/randomuser/core/extensions/Lazy.kt
+++ core/src/main/java/xyz/pavelkorolev/randomuser/core/extensions/Lazy.kt
@@ -1,3 +1,6 @@
 package xyz.pavelkorolev.randomuser.core.extensions
 
+/**
+ * Non synchronized lazy implementation
+ */
 fun <T> lazyUi(initializer: () -> T) = lazy(LazyThreadSafetyMode.NONE, initializer)
