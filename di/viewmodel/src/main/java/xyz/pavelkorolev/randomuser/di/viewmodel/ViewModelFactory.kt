d05436bd0d3fac6fe089bb2e0cd43532c5b213fc
Pavel Korolev <king64rus@gmail.com>
2020-08-07 21:28:19 +0300

    ViewModel DI module docs added.

--- di/viewmodel/src/main/java/xyz/pavelkorolev/randomuser/di/viewmodel/ViewModelFactory.kt
+++ di/viewmodel/src/main/java/xyz/pavelkorolev/randomuser/di/viewmodel/ViewModelFactory.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.ViewModelProvider
 import javax.inject.Inject
 import javax.inject.Provider
 
+/**
+ * Custom [ViewModelProvider.Factory] to inject [ViewModel] classes using Map multi-binding.
+ */
 class ViewModelFactory @Inject constructor(
     private val viewModels: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
--- di/viewmodel/src/main/java/xyz/pavelkorolev/randomuser/di/viewmodel/ViewModelKey.kt
+++ di/viewmodel/src/main/java/xyz/pavelkorolev/randomuser/di/viewmodel/ViewModelKey.kt
@@ -4,6 +4,10 @@ import androidx.lifecycle.ViewModel
 import dagger.MapKey
 import kotlin.reflect.KClass
 
+/**
+ * Annotation to mark [ViewModel] keys in multi-binding map.
+ * Used with @IntoMap annotation.
+ */
 @Target(
     AnnotationTarget.FUNCTION,
     AnnotationTarget.PROPERTY_GETTER,
