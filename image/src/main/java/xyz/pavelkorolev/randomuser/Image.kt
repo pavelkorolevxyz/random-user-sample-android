ed1202262032aa4c1bd6ebe0edb1c0fc245ea596
Pavel Korolev <king64rus@gmail.com>
2020-08-07 21:27:34 +0300

    Image module docs added.

--- image/src/main/java/xyz/pavelkorolev/randomuser/Image.kt
+++ image/src/main/java/xyz/pavelkorolev/randomuser/Image.kt
@@ -2,7 +2,19 @@ package xyz.pavelkorolev.randomuser
 
 import androidx.annotation.DrawableRes
 
+/**
+ * Image wrapper.
+ * Can be used to abstract image source.
+ */
 sealed class Image {
+
+    /**
+     * Image from remote url
+     */
     data class Url(val url: String) : Image()
+
+    /**
+     * Image from drawable resource
+     */
     data class Resource(@DrawableRes val resId: Int) : Image()
 }
--- image/src/main/java/xyz/pavelkorolev/randomuser/ImageCrop.kt
+++ image/src/main/java/xyz/pavelkorolev/randomuser/ImageCrop.kt
@@ -1,6 +1,17 @@
 package xyz.pavelkorolev.randomuser
 
+/**
+ * Crop mode for image loading
+ */
 sealed class ImageCrop {
+
+    /**
+     * No crop at all
+     */
     object None : ImageCrop()
+
+    /**
+     * Crop image as circle
+     */
     object Circle : ImageCrop()
 }
